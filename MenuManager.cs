a6cf413bc0675bccd02f394c95096d231b8628b0
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2019-03-21 16:15:30 -0400

    Create MenuManager.cs

---
+++ MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+
+    public Button btnStart;
+    public Button quit;
+    bool start = false;
+ 
+    // Use this for initialization
+    void Start()
+    {
+
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        quit.onClick.AddListener(QuitGame);
+        if (SceneManager.GetActiveScene().buildIndex != 1)
+        {
+            btnStart.onClick.AddListener(StartGame);
+           
+            if (start == true)
+            {
+                Debug.Log("hello");
+
+                SceneManager.LoadScene("SampleScene");
+                SceneManager.LoadScene(1);
+                Invoke("MainMenu", 1f);
+            }
+        }
+
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    public void StartGame()
+    {
+        start = true;
+     
+       
+       
+    }
+}
